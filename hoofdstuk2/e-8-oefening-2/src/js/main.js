390e5fa3181dbc313ac040e32e81091d3844514a
Tom Vanhoutte <syntraprogrammeurs@gmail.com>
2025-11-24 11:27:05 +0100

    hoofstuk 2 oef 3

--- hoofdstuk2/e-8-oefening-2/src/js/main.js
+++ hoofdstuk2/e-8-oefening-2/src/js/main.js
@@ -12,3 +12,50 @@ import * as bootstrap from 'bootstrap'
 •	add(amount), subtract(amount), reset()
 •	centrale render()
 */
+const Ex2CalculatorModule = (function () {
+
+    let total = 0;
+
+    function add(amount) {
+        total += amount;
+        render();
+    }
+
+    function subtract(amount) {
+        total -= amount;
+        render();
+    }
+
+    function reset() {
+        total = 0;
+        render();
+    }
+
+    function render() {
+        document.querySelector("#ex2_total").textContent = total;
+    }
+
+    return {
+        add,
+        subtract,
+        reset
+    };
+
+})();
+
+// events koppelen
+document
+    .querySelector("#ex2_add2")
+    .addEventListener("click", () => Ex2CalculatorModule.add(2));
+
+document
+    .querySelector("#ex2_add10")
+    .addEventListener("click", () => Ex2CalculatorModule.add(10));
+
+document
+    .querySelector("#ex2_sub4")
+    .addEventListener("click", () => Ex2CalculatorModule.subtract(4));
+
+document
+    .querySelector("#ex2_reset")
+    .addEventListener("click", Ex2CalculatorModule.reset);
---
+++ hoofdstuk2/f-8-oefening-3/src/js/main.js
@@ -0,0 +1,14 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap'
+
+//eigen js
+
+/*
+Je bouwt een module die:
+•	taken bewaart in een array
+•	taken rendert in een lijst
+•	via een filter-input taken live filtert
+*/
